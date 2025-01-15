ec3ee96a2fa714e0fae53345702bbc217300ccdc
rosariohdz1012 <hernandezvillegasm12@gmail.com>
2025-01-15 12:38:34 -0600

    Actulizacion de proyectos dia 3 a 6

--- 01-Java-Basico/HolaMundo/src/holamundo/HolaMundo.java
+++ 01-Java-Basico/HolaMundo/src/holamundo/HolaMundo.java
@@ -1,11 +1,10 @@
-package holamundo;
+package HolaMundo;
 
 public class HolaMundo {
 
     public static void main(String[] args) {
-
-        System.out.println("Hola Mundo");
-        //System.out.println("Probando un hola mundo diferente");
+              System.out.println("Hola Mundo");
+       // System.out.println("Probando un hola mundo diferente");
     }
-
+    
 }
